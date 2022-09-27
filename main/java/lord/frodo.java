4334b5543dc602bff0673d96dfc6543d0e390c2b
Artem Chernenkov <artemchernenkov9099@gmail.com>
2022-09-27 08:31:57 +0300

    Init

---
+++ main/java/lord/frodo.java
@@ -0,0 +1,7 @@
+package lord;
+
+public class frodo {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ test/java/test/math/film.java
@@ -0,0 +1,7 @@
+package test.math;
+
+public class film {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
